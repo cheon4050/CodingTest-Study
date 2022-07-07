a459e0c651c6752cd0ef44ebd3755f48603e6d86
Kimseongeun <62641359+FEKimseongeun@users.noreply.github.com>
2022-07-07 12:31:13 +0900

    Update kse_17626.py

--- 12주차/17626/kse_17626.py
+++ 12주차/17626/kse_17626.py
@@ -1,15 +1,13 @@
 n = int(input())
+d = [0] * (n + 1)
+d[0], d[1] = 0, 1
 
-dp = [0] * 50001
-dp[1] = 1
+for i in range(2, n + 1):
+    minValue = 1e9
+    j = 1
+    while (j ** 2) <= i:
+        minValue = min(minValue, d[i - (j ** 2)])
+        j += 1
+    d[i] = minValue + 1
 
-for i in range(2, n+1):
-    m = 50001 ** 2
-    mul = 1
-    while i - mul**2 >= 0:
-        m = min(m, dp[i - mul**2])
-        mul += 1
-
-    dp[i] = m + 1
-
-print(dp[n])
+print(d[n])
