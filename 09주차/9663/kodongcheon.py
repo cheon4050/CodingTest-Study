c8dc06e37b2076aff0a9ad7a53dbe40757b5ec7e
cheon4050 <ehdcjs159@gmail.com>
2022-09-14 01:49:01 +0900

    2022/09/14

--- 09주차/9663/kodongcheon.py
+++ 09주차/9663/kodongcheon.py
@@ -1,10 +1,29 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-visited = [[False] * N for _ in range(N)]
-
-def fun(visited,cnt):
+arr = [[1] * N for i in range(N)]
+result = 0
+def dfs(cnt, arr):
+    if cnt == N-1:
+        global result
+        result += sum(arr[cnt])
+        return
     for i in range(N):
-        if not visited[cnt][i]:
+        if arr[cnt][i]:
+            temp = [a[:]for a in arr]
+            for k in range(N):
+                temp[cnt][k] = 0
+                temp[k][i] = 0
+                if cnt + k <N and i+k<N:
+                    temp[cnt+k][i+k] = 0
+                if cnt + k < N and 0 <= i - k:
+                    temp[cnt+k][i-k] = 0
+                if 0 <= cnt - k and i + k < N:
+                    temp[cnt-k][i+k] = 0
+                if 0 <= cnt - k and 0 <= i - k:
+                    temp[cnt-k][i-k] = 0
+            dfs(cnt+1, temp)
 
-def aa(visited, x, y):
-    for i in range(x):
-
+dfs(0, arr)
+print(result)
